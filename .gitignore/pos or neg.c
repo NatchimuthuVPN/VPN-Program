d7bbe18c36c395b861290b7ff930b35a14a16946
NatchimuthuVPN <30368766+NatchimuthuVPN@users.noreply.github.com>
2017-08-11 16:46:26 +0530

    Update and rename .gitignore to pos or neg.c

---
+++ .gitignore/pos or neg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i;
+scanf("%d",&i);
+if(i<0)
+{
+printf("Negative\n");
+}
+else if(i=0)
+{
+printf("Zero\n");
+}
+else if(i>0)
+{
+printf("Positive\n");
+}
+return 0;
+}
